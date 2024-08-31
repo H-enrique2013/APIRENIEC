70d11d31769f624bd46a95a45528228155e4fc70
Enrique <hsandovals@uni.pe>
2024-08-31 17:31:35 -0500

    sd

--- main.py
+++ main.py
@@ -139,6 +139,10 @@ def cargamasivaplantillaDNI_Tipo1():
 def favicon():
     return send_from_directory('static', 'geopandasIcon.ico')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    try:
+        app.run(host='0.0.0.0', port=8000)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
 
