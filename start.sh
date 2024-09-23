00fd757e18724779c728a8e7fd1c48a3cbf321e0
Enrique <hsandovals@uni.pe>
2024-09-22 19:08:24 -0500

    nuevos cambios

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Iniciar la aplicación Flask con Gunicorn
-gunicorn -b 0.0.0.0:8000 main:app --timeout 120 --access-logfile /tmp/gunicorn_access.log --error-logfile /tmp/gunicorn_error.log
+gunicorn -b 0.0.0.0:8000 main:app --timeout 180 --access-logfile /tmp/gunicorn_access.log --error-logfile /tmp/gunicorn_error.log
