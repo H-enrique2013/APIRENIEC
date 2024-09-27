85b473c52cf4e6bd8bb08e85071b831983ce8fab
Enrique <hsandovals@uni.pe>
2024-09-27 16:38:22 -0500

    nuevos cambios

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory, render_template, redirect, g
+from flask import Flask, request, jsonify, send_from_directory, render_template
 from werkzeug.utils import secure_filename
 import pandas as pd
 from controllers.main_window import ListBookWindow
@@ -6,10 +6,13 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StringType
 import atexit
 import logging
+import os
+from pyspark import StorageLevel  # Importar StorageLevel
+
+# Configurar logging
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__, template_folder='templates')
-
-
 
 class Api_Reniec():
     
@@ -41,18 +44,18 @@ class Api_Reniec():
             .schema(schema) \
             .csv("/data/reniec.txt") \
             .repartition(2) \
-            .cache()  # Cachear el DataFrame
+            .persist(StorageLevel.DISK_ONLY)  # Cambiado a persistir en disco
         
         # Ejecutar una acción para cachear
         self.df.count()  # Acción que activa el cacheo
-        self.list_book_window=ListBookWindow()
-        
-    def validar_archivo_excel(self, archivo, archivo_nombre):
-        """Valida que el archivo subido sea un archivo de Excel válido"""
-        if archivo_nombre not in request.files:
-            return jsonify({"error": "No se ha subido ningún archivo"}), 400
+        self.list_book_window = ListBookWindow()
 
-        archivo = request.files[archivo_nombre]
+    def validar_archivo_excel(self, archivo_nombre):
+        """Valida que el archivo subido sea un archivo de Excel válido"""
+        archivo = request.files.get(archivo_nombre)
+
+        if not archivo:
+            return jsonify({"error": "No se ha subido ningún archivo"}), 400
 
         if archivo.filename == '':
             return jsonify({"error": "El archivo no tiene nombre"}), 400
@@ -71,111 +74,69 @@ class Api_Reniec():
         def favicon():
             return send_from_directory('static', 'Sispad.ico', as_attachment=False)
 
-        #Métodos POST
         @app.route('/consultaDNINombresApellidos', methods=['POST'])
         def consultaDNINombresApellidos():
             try:
                 data = request.get_json()
-                Typeconsulta = data.get('Typeconsulta', '')#Si es 0 busca por DNI y si es 1 busca por nomb y apellidos
+                Typeconsulta = data.get('Typeconsulta', '')
                 n_dni = data.get('N_DNI', '')
-                Nom = data.get('Nombres', '').upper()  # Convertir a mayúsculas
-                Ap_Pat = data.get('Ap_Paterno', '').upper()  # Convertir a mayúsculas
-                Ap_Mat = data.get('Ap_Materno', '').upper()  # Convertir a mayúsculas
+                Nom = data.get('Nombres', '').upper()
+                Ap_Pat = data.get('Ap_Paterno', '').upper()
+                Ap_Mat = data.get('Ap_Materno', '').upper()
 
-                if Typeconsulta!='0' and Typeconsulta!='1':
-                    return jsonify({"error":f"Typeconsulta debe ser solo 0 o 1: {Typeconsulta}"})
+                if Typeconsulta not in ['0', '1']:
+                    return jsonify({"error": f"Typeconsulta debe ser solo 0 o 1: {Typeconsulta}"}), 400
                 
-                if Typeconsulta=='0':
-                    # Validación del DNI
+                if Typeconsulta == '0':
                     if not n_dni or len(n_dni) != 8 or not n_dni.isdigit():
-                            return jsonify({"error": "DNI inválido. Debe ser un número de 8 dígitos"}), 400
+                        return jsonify({"error": "DNI inválido. Debe ser un número de 8 dígitos"}), 400
                     resultado = self.df.filter(self.df["DNI"] == n_dni)
-                elif Typeconsulta=='1':
-                    # Validación de campos:
-                    # Caso 1: Todos los campos deben ser diferentes de vacío
-                    # Caso 2: Nom puede estar vacío, pero Ap_Pat y Ap_Mat deben ser diferentes de vacío
+                elif Typeconsulta == '1':
                     if (not Nom and (not Ap_Pat or not Ap_Mat)) or (not Ap_Pat or not Ap_Mat):
                         return jsonify({"error": "Debe ingresar Ap_Paterno y Ap_Materno, y opcionalmente Nombres"}), 400
-                    if Nom != "" and Ap_Pat != "" and Ap_Mat != "":
+
+                    if Nom and Ap_Pat and Ap_Mat:
                         resultado = self.df.filter((self.df["NOMBRES"] == Nom) & (self.df["AP_PAT"] == Ap_Pat) & (self.df["AP_MAT"] == Ap_Mat))
-                    elif Nom == "" and Ap_Pat != "" and Ap_Mat != "":
+                    else:
                         resultado = self.df.filter((self.df["AP_PAT"] == Ap_Pat) & (self.df["AP_MAT"] == Ap_Mat))
-                # Selecciona solo las columnas requeridas
-                resultado_seleccionado = resultado.select("DNI", "NOMBRES", "AP_PAT", "AP_MAT", "FECHA_NAC", "DIRECCION", "EST_CIVIL", "MADRE", "PADRE","SEXO")
-                
-                # Convierte el DataFrame a una lista de tuplas
-                ResultadoConsulta = [tuple(row) for row in resultado_seleccionado.collect()]  
-                # Verificar si el resultado es válido
+
+                resultado_seleccionado = resultado.select("DNI", "NOMBRES", "AP_PAT", "AP_MAT", "FECHA_NAC", "DIRECCION", "EST_CIVIL", "MADRE", "PADRE", "SEXO")
+                ResultadoConsulta = [tuple(row) for row in resultado_seleccionado.collect()]
+
                 if ResultadoConsulta:
                     return jsonify(ResultadoConsulta), 200
                 else:
                     return jsonify({"error": "No se encontró información para los datos proporcionados"}), 404
             except Exception as e:
+                logging.error(f"Error en consultaDNINombresApellidos: {e}")
                 return jsonify({"error": f"Ha ocurrido un error: {str(e)}"}), 500
 
         @app.route('/cargamasivaDNI', methods=['POST'])
         def cargamasivaDNI():
-            archivo, error = self.validar_archivo_excel(request.files, 'archivo_excel')
+            archivo, error = self.validar_archivo_excel('archivo_excel')
             if error:
                 return error
             
             try:
-                # Leer el archivo Excel directamente en un DataFrame
-                df_archivo = pd.read_excel(archivo,dtype={'DNI': str})
-
-                lista_dni=df_archivo['DNI'].tolist()
-                # Filtrar el DataFrame por los números de DNI especificados
+                df_archivo = pd.read_excel(archivo, dtype={'DNI': str})
+                lista_dni = df_archivo['DNI'].tolist()
                 resultado = self.df.filter(self.df['DNI'].isin(lista_dni))
-                # Selecciona solo las columnas requeridas
-                resultado_seleccionado = resultado.select("DNI", "NOMBRES", "AP_PAT", "AP_MAT", "FECHA_NAC", "DIRECCION", "EST_CIVIL", "MADRE", "PADRE","SEXO")
-                # Convierte el DataFrame a una lista de tuplas
+                resultado_seleccionado = resultado.select("DNI", "NOMBRES", "AP_PAT", "AP_MAT", "FECHA_NAC", "DIRECCION", "EST_CIVIL", "MADRE", "PADRE", "SEXO")
                 ResultadoConsulta = [tuple(row) for row in resultado_seleccionado.collect()]
-                # Verificar si el resultado es válido
+
                 if ResultadoConsulta:
                     return jsonify(ResultadoConsulta), 200
                 else:
                     return jsonify({"error": "No se encontró información para los datos proporcionados"}), 404
-
             except Exception as e:
+                logging.error(f"Error en cargamasivaDNI: {e}")
                 return jsonify({"error": str(e)}), 500
 
-
-        @app.route('/cargamasivaplatillaDNI', methods=['POST'])
-        def cargamasivaplantillaDNI():
-            archivo, error = self.validar_archivo_excel(request.files, 'archivoplantilla_excel')
-            if error:
-                return error
-
-            try:
-                # Leer el archivo Excel directamente en un DataFrame
-                df_archivo = pd.read_excel(archivo,dtype={'DNI': str})
-                lista_dni=df_archivo['DNI'].tolist()
-                # Filtrar el DataFrame por los números de DNI especificados
-                resultado = self.df.filter(self.df['DNI'].isin(lista_dni))
-                # Selecciona solo las columnas requeridas
-                resultado_selec = resultado.select("DNI", "AP_PAT", "AP_MAT","NOMBRES","SEXO","FECHA_NAC", "DIRECCION","UBIGEO_DIR","UBIGEO_NAC","EST_CIVIL","PADRE", "MADRE")
-                
-                # Supongamos que la función `seleccionar_archivo_xlsx` hace algún tipo de procesamiento
-                plantilla= self.list_book_window.seleccionar_archivo_Plantilla_xlsx(df_archivo,resultado_selec)
-                # Realiza operaciones con list_book_window...
-                #list_book_window.stop_spark()
-                
-                # Verificar si el resultado es válido
-                if plantilla:
-                    return jsonify(plantilla), 200
-                else:
-                    return jsonify({"error": "No se encontró información para los datos proporcionados"}), 404
-
-            except Exception as e:
-                return jsonify({"error": str(e)}), 500
-            
         if __name__ == "__main__":
-            try:
-                app.run(host='0.0.0.0', port=8000)
-            except Exception as e:
-                print(f"Error occurred: {e}")
-
-
+            # Obtener el puerto del entorno, si está disponible
+            port = int(os.environ.get('PORT', 8000))
+            logging.info(f"Iniciando servidor en el puerto {port}")
+            app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == "__main__":
@@ -183,10 +144,11 @@ if __name__ == "__main__":
 
     @atexit.register
     def cerrar_spark():
-        api_reniec.df.unpersist()  # Liberar el caché
+        api_reniec.df.unpersist()
         api_reniec.spark.stop()
+        logging.info("Spark detenido y caché liberado")
 
     try:
         api_reniec.run()
     except Exception as e:
-        logging.error(f"Ocurrió un error: {e}")
+        logging.error(f"Ocurrió un error al ejecutar la aplicación: {e}")
